ff9c88d63d3ca59271a947f3ed5b3ae5dff1b950
dsh0416 <dsh0416@users.noreply.github.com>
2020-01-21 10:02:04 +0000

    Automated deployment: Tue Jan 21 10:02:04 UTC 2020 a70d0ac596dcf06b9b95450720ec269cc92d629c

--- tests/chapter01.rb
+++ tests/chapter01.rb
@@ -27,3 +27,40 @@ test 'check the combination of print puts and p' do
   end
   assert output == "HelloHello\n\"Hello\"\n"
 end
+
+# Chapter 01, Variables
+test 'check bool operations' do
+  assert (true and true) == true
+  assert (true and false) == false
+  assert (false and true) == false
+  assert (false and false) == false
+
+  assert (true or true) == true
+  assert (true or false) == true
+  assert (false or true) == true
+  assert (false or false) == false
+
+  assert (not true) == false
+  assert (not false) == true
+end
+
+# Chapter 01, Variables
+test 'check bool operations with symbols' do
+  assert (true && true) == true
+  assert (true && false) == false
+  assert (false && true) == false
+  assert (false && false) == false
+
+  assert (true || true) == true
+  assert (true || false) == true
+  assert (false || true) == true
+  assert (false || false) == false
+
+  assert !true == false
+  assert !false == true
+end
+
+# Chapter 01, Variables
+test 'check bool operations excercise' do
+  assert (true && (true and not false) or !false || false) == true
+end
