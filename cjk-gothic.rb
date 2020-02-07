9cf8b60ab2dca67a790a72a2197d3d02a6c2a7a6
Delton Ding <dsh0416@gmail.com>
2020-02-08 08:37:52 +0900

    Fix CJK word breaking

--- cjk-gothic.rb
+++ cjk-gothic.rb
@@ -4,6 +4,18 @@ module Asciidoctor
       module CJKGothic
         VERSION = "0.0.1"
       end
+
+      def self.break_words(string)
+        string.gsub(/(?<!^|\p{Space}|\p{Ps}|\p{Pi})[\p{Han}\p{Hiragana}\p{Katakana}\p{Ps}\p{Pi}]/) {|s| "#{::Prawn::Text::ZWSP}#{s}"}
+      end
+    end
+
+    class Converter
+      def typeset_text_with_break_words(string, line_metrics, opts = {})
+        typeset_text_without_break_words ::Asciidoctor::Pdf::CJK.break_words(string), line_metrics, opts
+      end
+      alias_method :typeset_text_without_break_words, :typeset_text
+      alias_method :typeset_text, :typeset_text_with_break_words
     end
 
     class ThemeLoader
