d2bb3658daf8e23b68922063d0d552e664348f54
Delton Ding <dsh0416@gmail.com>
2020-02-08 08:27:13 +0900

    Add fonts

--- cjk-gothic.rb
+++ cjk-gothic.rb
@@ -1,7 +1,3 @@
-require "asciidoctor/pdf/cjk"
-require "asciidoctor/pdf/cjk/kai_gen_gothic/version"
-require "asciidoctor/pdf/cjk/kai_gen_gothic/theme_loader.rb"
-
 module Asciidoctor
   module Pdf
     module CJK
